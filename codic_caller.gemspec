8ac42ace920bbca539bc4d4102fcacf2d2249319
HaiTo <kimura@sansan.com>
2015-10-01 16:11:47 +0900

    ok

--- codic_caller.gemspec
+++ codic_caller.gemspec
@@ -30,6 +30,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.10"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency 'rubocop'
+  spec.add_development_dependency 'pry-byebug'
   spec.add_runtime_dependency 'faraday'
   spec.add_runtime_dependency 'faraday_middleware'
 end
--- lib/codic_caller.rb
+++ lib/codic_caller.rb
@@ -1,5 +1,44 @@
 require "codic_caller/version"
+require 'faraday'
+require 'faraday_middleware'
+require 'json'
 
 module CodicCaller
-  # Your code goes here...
+  class Codic
+    HOST = 'https://api.codic.jp'
+
+    def initialize
+      @token = ENV['CODIC']
+    end
+
+    def translate(text, casing: 'lower underscore')
+      JSON.load(
+        client.get {|req|
+          req.url '/v1/engine/translate.json'
+          req.params[:project_id] = project_id
+          req.params[:casing] = casing
+          req.params[:text] = text
+          req.headers['Authorization'] = "Bearer #{@token}"
+        }.body
+      ).first['translated_text']
+    end
+
+    def project_id
+      JSON.load(
+        client.get {|req|
+          req.url '/v1/user_projects.json'
+          req.headers['Authorization'] = "Bearer #{@token}"
+        }.body
+      ).first['id']
+    end
+
+    private
+
+    def client
+      @client ||= Faraday.new(url: HOST) do |faraday|
+        faraday.request :json
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+  end
 end
